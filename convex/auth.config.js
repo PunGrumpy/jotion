ade1d063217891327487e9e22c0241a81fd7f4e0
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-25 23:49:11 +0700

    fix(convex): update dependencies convex

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://merry-penguin-39.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
